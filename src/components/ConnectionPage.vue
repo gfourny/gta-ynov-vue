26be43d62a9fbc133fc1739bbd55e5180b694a51
gfourny <fourny.g@free.fr>
2018-11-08 13:54:57 +0100

    première mise en prod

---
+++ src/components/ConnectionPage.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="LoginPage">
+        coucou
+    </div>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ConnectionPage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import ConnectionPage from "@/components/ConnectionPage.vue";
+
+import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue);
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 export default {
   name: "home",
   components: {
-    HelloWorld
+    ConnectionPage
   }
 };
 </script>
