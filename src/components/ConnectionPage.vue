afbe299d1fbe8a102555eb3d6dfd0453e6d231b6
gfourny <fourny.g@free.fr>
2018-11-08 15:59:15 +0100

    modification login page, ajout du status

--- src/components/ConnectionPage.vue
+++ src/components/ConnectionPage.vue
@@ -10,6 +10,15 @@
                 <label for="exampleInputPassword1">Password</label>
                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" style="width:25%; margin-left: 38%">
             </div>
+            <div class="form-group">
+                <label for="statusPersonne">Votre status</label>
+                <select class="form-control" style="width:25%; margin-left: 38%">
+                        <option disabled value="">Status</option>
+                        <option>Salarié</option>
+                        <option>Responsable d’équipe</option>
+                        <option>DRH</option>
+                </select>
+            </div>
             <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
