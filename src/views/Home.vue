7d81883e5887b131d8f6eaa3578976b7682b32ed
gfourny <guillaume.fourny@ynov.com>
2018-11-15 14:13:23 +0100

    modification logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-    <img alt="GTA" src="../assets/GTA.png">
+    <img alt="GTA" src="../assets/GTA-logo.png" style="width: 200px">
     <ConnectionPage msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
